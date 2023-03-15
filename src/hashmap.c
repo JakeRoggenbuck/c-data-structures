37fb14a5ab2b926868f1262aee51f669f085f278
Jake <jakeroggenbuck2@gmail.com>
2023-03-15 12:42:54 -0700

    Add hashmap add and get

--- src/hashmap.c
+++ src/hashmap.c
@@ -1,25 +1,55 @@
-#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define COUNT UINT_MAX
+#define COUNT 1000000
+
+unsigned int fnv1a(char *string);
+
+struct Value {
+    char *data;
+};
 
 struct Pair {
     size_t id;
     char *key;
-    char *value;
+    struct Value *value;
 };
 
 struct Hashmap {
     struct Pair **buckets;
 };
 
+struct Value *build_value(char *data) {
+    struct Value *v = malloc(sizeof(struct Value));
+    v->data = data;
+    return v;
+}
+
+struct Pair *build_pair(char *key, struct Value *v) {
+    struct Pair *p = malloc(sizeof(struct Pair));
+    p->id = fnv1a(key);
+    p->key = key;
+    p->value = v;
+
+    return p;
+}
+
 struct Hashmap *build_hashmap(int size) {
     struct Hashmap *h = malloc(sizeof(struct Hashmap));
-    h->buckets = malloc(size * sizeof(struct Pair *));
+    h->buckets = malloc(size * sizeof(struct Pair));
     for (int i = 0; i < size; ++i) {
         h->buckets[i] = NULL;
     }
     return h;
+}
+
+void add(struct Hashmap *h, char *value, struct Value *v) {
+    struct Pair *p = build_pair(value, v);
+    h->buckets[p->id] = p;
+}
+
+struct Pair *get(struct Hashmap *h, char *key) {
+    unsigned int i = fnv1a(key);
+    return h->buckets[i];
 }
 
 unsigned int fnv1a(char *string) {
@@ -32,10 +62,21 @@ unsigned int fnv1a(char *string) {
         string++;
     }
 
-    return hash;
+    return hash % COUNT;
 }
 
 int main() {
+	printf("NOTE: there may be collisions.\n");
     struct Hashmap *h = build_hashmap(COUNT);
+
+    struct Value *v = build_value("myvalue");
+    add(h, "key", v);
+
+    struct Pair *p = get(h, "key");
+    if (p != NULL) {
+        printf("Value: \"%s\" found!\n", p->value->data);
+    } else {
+        printf("No value found.\n");
+    }
     return 0;
 }
